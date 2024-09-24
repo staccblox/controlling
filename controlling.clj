2aa2b9027bbd86492475179c05c42e0ef0efac43
staccblox <67884277+staccblox@users.noreply.github.com>
2024-09-24 13:21:30 -0400

    Update controlling.clj

--- controlling.clj
+++ controlling.clj
@@ -1,5 +1,5 @@
-(ns clojure.examples.hello
+(ns clojure.examples.controlling
    (:gen-class))
-(defn hello-world []
-   (println "Hello World"))
-(hello-world)
+(defn controlling []
+   (println "A large percentage of a manager’s time is spent controlling the activities within the business to ensure that it’s on track to achieve its goals. When people or processes stray from the path, managers are often the first ones to notice and take corrective action."))
+(controlling)
